12c41809f6280330d3d8e7a9a41e07dc463c4a4c
sh.lee <sh.lee@zespro.co.kr>
2015-07-16 14:48:10 +0900

    create project add index blade page add Foundation library

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Stigma2</title>
+
+        <link rel="stylesheet" href="css/app.css" />
+
+        <script src="bower_components/modernizr/modernizr.js"></script>
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/foundation/js/foundation.min.js"></script>
+    </head>
+    <body>
+        <h1>Index blade page. haha~</h1>
+    </body>
+</html>
