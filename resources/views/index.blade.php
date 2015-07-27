16341d246c6d553b88ed0cfeb7f914d0282a927c
shlee <shlee@adcloudsystem.com>
2015-07-27 16:17:53 +1000

    add buttons, offcanvas scss modify index side menu by offcanvas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,50 @@
                 </top-bar>
             </div>
         </header>
-        <div class="row">
+        <div class="off-canvas-wrap" data-offcanvas>
+          <div class="inner-wrap">
+            <nav class="tab-bar">
+              <section class="left-small">
+                <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+              </section>
+
+              <section class="middle tab-bar-section">
+                <h1 class="title">Foundation</h1>
+              </section>
+
+              <section class="right-small">
+                <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+              </section>
+            </nav>
+
+            <aside class="left-off-canvas-menu">
+              <ul class="off-canvas-list">
+                <li><label>Foundation</label></li>
+                <li><a href="#">The Psychohistorians</a></li>
+                <li><a href="#">...</a></li>
+              </ul>
+            </aside>
+
+            <aside class="right-off-canvas-menu">
+              <ul class="off-canvas-list">
+                <li><label>Users</label></li>
+                <li><a href="#">Hari Seldon</a></li>
+                <li><a href="#">...</a></li>
+              </ul>
+            </aside>
+
+            <section class="main-section">
+                <!-- content goes here -->
+                <article>
+                    <h1>Main Contents</h1>
+                    <div ui-view></div>
+                </article>
+            </section>
+          <a class="exit-off-canvas"></a>
+
+          </div>
+        </div>
+        <!-- <div class="row">
             <div class="large-2 columns">
                 <aside class="sidebar">
                     <nav>
@@ -86,7 +129,7 @@
                     <div ui-view></div>
                 </article>
             </div>
-        </div>
+        </div> -->
         <footer class="row">
             <div class="large-12 columns">
                 <hr/>
