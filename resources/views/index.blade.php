39d38b3881deea37fea1328cff10502d6deb1e29
shlee <shlee@adcloudsystem.com>
2015-07-28 19:15:52 +1000

    modify active background color modify index header for foundation and mm.foundation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,43 +9,64 @@
         <script data-main="scripts/main" src="bower_components/requirejs/require.js"></script>
     </head>
     <body>
-        <div ng-controller="TopBarDemoCtrl">
-            <top-bar>
-                <ul class="title-area">
-                  <li class="name">
-                    <h1><a href="#">My Site</a></h1>
-                  </li>
-                  <li toggle-top-bar class="menu-icon"><a href="#">Menu</a></li>
-                </ul>
-
-                <top-bar-section>
-                    <!-- Left Nav Section -->
-                    <ul class="left">
-                        <li><a href="#"><i class="fi-list"></i></a></li>
+        <header>
+            <div  ng-controller="TopBarDemoCtrl">
+                <top-bar>
+                <nav class="top-bar" data-topbar role="navigation">
+                    <ul class="title-area">
+                        <li class="name">
+                            <h1><a href="#">My Site</a></h1>
+                        </li>
                     </ul>
-
-                    <!-- Right Nav Section -->
-                    <ul class="right">
-                    <li class="active"><a href="#">Active</a></li>
-                    <li has-dropdown>
-                      <a href="#">Dropdown</a>
-                      <ul top-bar-dropdown>
-                        <li><a href="#">1 link in dropdown</a></li>
-                        <li><a href="#">2 link in dropdown</a></li>
-                        <li><a href="#">3 link in dropdown</a></li>
-                        <li><a href="#">4 link in dropdown</a></li>
-                        <li><a href="#">5 link in dropdown</a></li>
-                        <li><a href="#">6 link in dropdown</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="#"><i class="fi-widget"></i></a></li>
-                    <li><a href="#"><i class="fi-lock"></i></a></li>
-                    </ul>
-                </top-bar-section>
-            </top-bar>
-        </div>
-        <!-- <div ui-view></div> -->
-
-    </div>
+                    <top-bar-section>
+                    <section class="top-bar-section">
+                        <ul class="left">
+                            <li><a href="#"><i class="fi-list"></i></a></li>
+                            <li><a href="#">Left Nav Button</a></li>
+                        </ul>
+                        <ul class="right">
+                            <li class="active"><a href="#">Right Button Active</a></li>
+                            <li has-dropdown>
+                                <a href="#">Right Button Dropdown</a>
+                                <ul class="dropdown">
+                                    <li><a href="#">First link in dropdown</a></li>
+                                    <li class="active"><a href="#">Active link in dropdown</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#"><i class="fi-widget"></i></a></li>
+                            <li><a href="#"><i class="fi-lock"></i></a></li>
+                        </ul>
+                    </section>
+                    </top-bar-section>
+                </nav>
+                </top-bar>
+            </div>
+        </header>
+        <aside>
+            <div class="icon-bar vertical five-up">
+                <a class="item">
+                    <i class="fi-home"></i>
+                </a>
+                <a class="item">
+                    <i class="fi-bookmark"></i>
+                </a>
+                <a class="item">
+                    <i class="fi-info"></i>
+                </a>
+                <a class="item">
+                    <i class="fi-mail"></i>
+                </a>
+                <a class="item">
+                    <i class="fi-like"></i>
+                </a>
+            </div>
+        </aside>
+        <section>
+            <article>
+                <!-- <div ui-view></div> -->
+            </article>
+        </section>
+        <footer>
+        </footer>
     </body>
 </html>
