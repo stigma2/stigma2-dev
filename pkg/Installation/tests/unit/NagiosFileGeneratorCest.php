3b0d79d20d76989738c4ca6949b11cbd767fce3f
jaehee <jaehee@artgrafii.com>
2015-09-22 14:14:27 +1000

    bugfix

--- pkg/Installation/tests/unit/NagiosFileGeneratorCest.php
+++ pkg/Installation/tests/unit/NagiosFileGeneratorCest.php
@@ -16,5 +16,18 @@ class NagiosFileGeneratorCest
     // tests
     public function testFileGenerate(UnitTester $I)
     {
+        $data = [
+            'host'=> 'localhost',
+            'port'=>'80'
+        ];
+
+        $fileGenerator = new NagiosFileGenerator(__DIR__.'../../../src/tmpl/nagios.php',config_path().'/nagios.php') ; 
+        $fileGenerator->make($data) ;
+
+        $stubFile = file_get_contents(__DIR__.'/../'.'_stubs/nagios.php') ;
+        $createdDbFile = file_get_contents(config_path().'/nagios.php') ;
+
+        $I->assertFileExists((config_path().'/nagios.php')) ; 
+        $I->assertEquals($stubFile, $createdDbFile) ; 
     }
 }
