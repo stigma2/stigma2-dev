2c3660cef7940b18566212ac6c5a92ce8e514342
shlee <shlee@adcloudsystem.com>
2015-09-24 05:52:18 +0000

    modify how to get grafana dashboard

--- public/scripts/services/ServerServiceFactory.js
+++ public/scripts/services/ServerServiceFactory.js
@@ -5,11 +5,11 @@ define(['./implement', './module', '../config'],
         services.factory('ServerServiceFactory', function($http) {
             return {
                 list: function(status) {
-                    var url = config.get().home + '/api/server/services?status=' + status;
+                    var url = config.get().home + '/api/v1/server/services?status=' + status;
                     return implement.httpGetServiceImpl($http, url);
                 },
                 show: function(service_name) {
-                    var url = config.get().home + '/api/server/services/' + service_name;
+                    var url = config.get().home + '/api/v1/server/services/' + service_name;
                     return implement.httpGetServiceImpl($http, url);
                 },
             }
