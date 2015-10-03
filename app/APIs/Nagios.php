014b4a27bfdc0dd6dfb078d3ba18a1812c8c888a
shlee <shlee@adcloudsystem.com>
2015-10-03 01:45:09 +0000

    delete temp sources

--- app/APIs/Nagios.php
+++ app/APIs/Nagios.php
@@ -30,10 +30,8 @@ class Nagios implements NagiosInterface
 
     public function listServices($status)
     {
-        // $command = "statusjson.cgi?query=servicelist&details=true";
         $command = "api/v1/services";
         if (isset($this->_service_status[$status])) {
-            // $command = "statusjson.cgi?query=servicelist&details=true&servicestatus=".$this->_service_status[$status];
             $command = "api/v1/services?servicestatus=".$this->_service_status[$status];
         }
         $result = $this->call($command);
@@ -51,24 +49,14 @@ class Nagios implements NagiosInterface
 
     private function call($command)
     {
-        // $domain = "http://106.243.134.121:9090/nagios/cgi-bin/";
-        $domain = "http://106.243.134.121:22180/nagios_dev/";
-        // $domain = env("NAGIOS_DOMAIN");
+        $domain = env("NAGIOS_DOMAIN");
         $url = $domain.$command;
-
-        // $port = "22180";
-        // $timeout = "3";
-        // $port = env("NAGIOS_PORT");
-        // $timeout = env("NAGIOS_TIMEOUT");
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_PORT, $port);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_COOKIE, "");
         curl_setopt($ch, CURLOPT_TIMEOUT, "3");
-
-        // curl_setopt($ch, CURLOPT_USERPWD, "nagiosadmin:qwe123");
 
         $result = curl_exec($ch);
         curl_close($ch);
