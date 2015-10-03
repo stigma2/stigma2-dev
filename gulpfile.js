df5a130ed9d1f2b82e8af949ba667ce5e2ce02df
shlee <shlee@adcloudsystem.com>
2015-10-03 09:15:10 +0000

    Merge

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-<<<<<<< HEAD
-    mix.copy('public/bower_components/foundation/scss/foundation', 'resources/assets/sass/foundation');
-    mix.sass('app.scss');
-});
-=======
     mix.copy('public/bower_components/foundation/scss', 'resources/assets/sass');
     mix.sass(['app.scss']);
     mix.version('public/css/app.css') ; 
     //mix.task('webpack-dev-server');
 }); 
-
->>>>>>> 5cf0b026c1fc859aed6eca7694fa5301b95c32bf
