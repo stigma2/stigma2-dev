f0cf263b4d8679a6fd94b70cc733b4ee334d9dd8
jaehee <jaehee@artgrafii.com>
2015-10-05 00:27:09 +1100

    update

--- app/Http/Routes/admin.php
+++ app/Http/Routes/admin.php
@@ -4,4 +4,5 @@ Route::group(array('prefix'=>'admin'),function(){
     Route::resource('hosts','\App\Http\Controllers\Admin\HostController');
     Route::resource('services','\App\Http\Controllers\Admin\ServiceController');
     Route::resource('commands','\App\Http\Controllers\Admin\CommandController');
+    Route::resource('dashboard','\App\Http\Controllers\Admin\DashboardController');
 });
