451c9846c4fb2e8df63cb3e799852fc2ef98d954
shlee <shlee@adcloudsystem.com>
2015-10-28 06:06:54 +0000

    modified modal event handler added save, cancel button on modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,52 +52,54 @@
         </footer>
         <div id="indexConfigArea" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
             <h2 id="modalTitle">Configuration</h2>
-            <p>
-                <form>
+            <p></p>
+            <div class="row">
+                <div class="medium-10 columns">
                     <div class="row">
-                        <div class="medium-10 columns">
-                            <div class="row">
-                                <div class="medium-3 columns">
-                                    <label for="right-label" class="right inline">Auto Refresh</label>
-                                </div>
-                                <div class="medium-9 columns">
-                                    <select ng-model="refreshInterval" ng-change="setRefreshInterval();">
-                                        <option value="15000">Every 15 s</option>
-                                        <option value="30000">Every 30 s</option>
-                                        <option value="60000">Every 1 m</option>
-                                        <option value="300000">Every 5 m</option>
-                                        <option value="900000">Every 15 m</option>
-                                        <option value="1800000">Every 30 m</option>
-                                        <option value="3600000">Every 1 h</option>
-                                        <option value="7200000">Every 2 h</option>
-                                        <option value="86400000">Every 1 d</option>
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="medium-3 columns">
+                            <label for="right-label" class="right inline">Auto Refresh</label>
+                        </div>
+                        <div class="medium-9 columns">
+                            <select ng-model="refreshInterval">
+                                <option value="15000">Every 15 s</option>
+                                <option value="30000">Every 30 s</option>
+                                <option value="60000">Every 1 m</option>
+                                <option value="300000">Every 5 m</option>
+                                <option value="900000">Every 15 m</option>
+                                <option value="1800000">Every 30 m</option>
+                                <option value="3600000">Every 1 h</option>
+                                <option value="7200000">Every 2 h</option>
+                                <option value="86400000">Every 1 d</option>
+                            </select>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="medium-10 columns">
                     <div class="row">
-                        <div class="medium-10 columns">
-                            <div class="row">
-                                <div class="medium-3 columns">
-                                    <label for="right-label" class="right inline">Event Lists</label>
-                                </div>
-                                <div class="medium-9 columns">
-                                    <select ng-model="overviewEventDurationDate" ng-change="setOverviewEventDurationDate();">
-                                        <option value="1">1 Day</option>
-                                        <option value="2">2 Days</option>
-                                        <option value="3">3 Days</option>
-                                        <option value="7">1 Week</option>
-                                        <option value="14">2 Weeks</option>
-                                        <option value="28">4 Weeks</option>
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="medium-3 columns">
+                            <label for="right-label" class="right inline">Event Lists</label>
+                        </div>
+                        <div class="medium-9 columns">
+                            <select ng-model="overviewEventDurationDate">
+                                <option value="1">1 Day</option>
+                                <option value="2">2 Days</option>
+                                <option value="3">3 Days</option>
+                                <option value="7">1 Week</option>
+                                <option value="14">2 Weeks</option>
+                                <option value="28">4 Weeks</option>
+                            </select>
                         </div>
                     </div>
-                </form>
-            </p>
-            <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="medium-10 columns">
+                    <a class="button right" ng-click="cancel()">Cancel</a>
+                    <a class="button success right" ng-click="save()">Save</a>
+                </div>
+            </div>
         </div>
     </body>
 </html>
